5649bd9363e40774f761ba573d5d4a98195f2a6f
Thien-Nam Dinh <thidinh@sandia.gov>
2025-07-02 13:49:45 -0600

    add networking configurations to reduce congestion

--- compose/ontology/run.sh
+++ compose/ontology/run.sh
@@ -5,9 +5,13 @@ if [ -z "$SECRET" ]; then
     exit 1
 fi
 
+if [ -z "$WINDOW" ]; then
+    WINDOW="#t"
+fi
+
 record=$( cat record.scm )
 control=$( cat control.scm )
 ledger=$( cat ledger.scm )
 ontology=$( cat ontology.scm )
 
-./journal-sdk -b "($record \"$SECRET\" $control ($ledger #t #f) $ontology)" -s "(*step* \"$SECRET\")" -p 80 -c $PERIODICITY
+./journal-sdk -b "($record \"$SECRET\" $control ($ledger $WINDOW #f) $ontology)" -s "(*step* \"$SECRET\")" -p 80 -c $PERIODICITY -l $DELAY
