05c697fd8731db0d71ba3619f3676ff94a7e6e2e
Thien-Nam Dinh <thidinh@sandia.gov>
2025-07-17 21:42:30 -0700

    Make RUST_LOG configurable from docker-compose for ledger and ontology services

--- compose/ontology/run.sh
+++ compose/ontology/run.sh
@@ -9,9 +9,13 @@ if [ -z "$WINDOW" ]; then
     WINDOW="#f"
 fi
 
+if [ -z "$RUST_LOG" ]; then
+    RUST_LOG="info"
+fi
+
 record=$( cat record.scm )
 control=$( cat control.scm )
 ledger=$( cat ledger.scm )
 ontology=$( cat ontology.scm )
 
-RUST_LOG=info ./journal-sdk -b "($record \"$SECRET\" $control ($ledger \"$SECRET\" #f $WINDOW) $ontology)" -s "(*step* \"$SECRET\")" -p 80 -c $PERIODICITY -d database
+RUST_LOG=$RUST_LOG ./journal-sdk -b "($record \"$SECRET\" $control ($ledger \"$SECRET\" #f $WINDOW) $ontology)" -s "(*step* \"$SECRET\")" -p 80 -c $PERIODICITY -d database
