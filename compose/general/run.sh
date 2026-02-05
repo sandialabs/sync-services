2f37f233f92f7b637cf34499c0391ea2081588fe
Thien-Nam Dinh <thidinh@sandia.gov>
2026-02-05 02:52:12 -0800

    prepare for testing

--- compose/general/run.sh
+++ compose/general/run.sh
@@ -9,8 +9,6 @@ if [ -z "$WINDOW" ]; then
     WINDOW="#f"
 fi
 
-cd ../../../sync-records/lisp 
-
 control=$( cat control.scm )
 standard=$( cat standard.scm )
 chain=$( cat log-chain.scm )
@@ -18,13 +16,9 @@ tree=$( cat tree.scm )
 configuration=$( cat configuration.scm )
 ledger=$( cat ledger.scm )
 
-cd -
-
 interface=$( cat interface.scm )
 
 boot="($interface '$SECRET '$SECRET $WINDOW $control $standard '$chain '$tree '$configuration '$ledger)"
 step="((function *step*) (arguments ()) (authentication $SECRET))"
 
-# RUST_LOG=$RUST_LOG ./journal-sdk -b "$script" -p 80 -c $PERIODICITY -d database
-# RUST_LOG=$RUST_LOG ./journal-sdk -b "$boot" -p 4096 -c $PERIODICITY -s "$step"
-RUST_LOG=$RUST_LOG ./journal-sdk -p 4096 -c $PERIODICITY
+RUST_LOG=$RUST_LOG ./journal-sdk -b "$boot" -p 4096 -c $PERIODICITY -s "$step" -d database
